84b2ea237846cbfca60da52049b5e034bafadfaa
Prabubas123 <prabubaskaran89@gmail.com>
2020-05-28 18:07:57 +0530

    Child component name changed

--- force-app/main/default/lwc/parentCmp/parentCmp.js
+++ force-app/main/default/lwc/parentCmp/parentCmp.js
@@ -3,6 +3,6 @@ import { LightningElement } from 'lwc';
 export default class ParentCmp extends LightningElement {
 
     handleChildMethod(event){
-        this.template.querySelector('c-child-cmp1').handleParenttoChild(event.target.value);
+        this.template.querySelector('c-child-cmp').handleParenttoChild(event.target.value);
     }
 }
